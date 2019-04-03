a889dd77997bd75953edf1859698c517c29368a9
RED <purred@udid.co.kr>
2019-04-03 21:02:17 +0900

    헬퍼 작업

---
+++ StartXamarin/StartXamarin/SubModule/Blog10/Helpers/LocationHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartXamarin.SubModule.Blog10.Helpers
+{
+    class LocationHelper
+    {
+    }
+}
---
+++ StartXamarin/StartXamarin/SubModule/Blog10/Helpers/PermissionsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace StartXamarin.SubModule.Blog10.Helpers
+{
+    public class PermissionsHelper
+    {
+        public static async Task<PermissionStatus> GetPermission(Permission permissionType)
+        {
+            // added using Plugin.Permissions;
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permissionType);
+            if (status != PermissionStatus.Granted)
+            {
+                // This is the actual permission request
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(permissionType);
+                if (results.ContainsKey(permissionType))
+                    status = results[permissionType];
+            }
+
+            return status;
+        }
+    }
+}
